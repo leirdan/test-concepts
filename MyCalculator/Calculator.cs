d3ce31a5724cfd7635a8443dbefed5527206e634
leirdan <uouleirdan@gmail.com>
2023-10-23 11:53:35 -0300

    :sparkles: feat: new methods on Calculator

--- MyCalculator/Calculator.cs
+++ MyCalculator/Calculator.cs
@@ -2,6 +2,8 @@
 
 public class Calculator()
 {
+    static void Main() { }
+
     public int Add(params int[] p)
     {
         int s = 0;
@@ -10,5 +12,34 @@ public class Calculator()
             s += i;
         }
         return s;
+    }
+    public int Subtract(params int[] p)
+    {
+        int s = p[0];
+        for (int i = 1; i < p.Length; i++)
+        {
+            s -= p[i];
+        }
+        return s;
+    }
+
+    public int Multiply(params int[] p)
+    {
+        int m = 1;
+        foreach (int i in p)
+        {
+            m *= i;
+        }
+        return m;
+    }
+
+    public int Divide(params int[] p)
+    {
+        int d = p[0];
+        for (int i = 0; i < p.Length - 1; i++)
+        {
+            d /= p[i + 1]; 
+        }
+        return d;
     }
 }
