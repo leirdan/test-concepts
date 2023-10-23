25815c33fe5fa06dc10be741c9134cc52b7be4b0
leirdan <uouleirdan@gmail.com>
2023-10-23 11:36:55 -0300

    :test_tube: tests: implementing test-driven-development

--- MyCalculator.Tests/CalculatorTests.cs
+++ MyCalculator.Tests/CalculatorTests.cs
@@ -15,4 +15,24 @@ public class CalculatorTests
         // Assert
         Assert.AreEqual(9, res);
     }
+    [TestMethod]
+    public void SubtractNumbers()
+    {
+        var calculator = new Calculator();
+        var res = calculator.Subtract(10, 3, 5);
+        Assert.AreEqual(2, res);
+    }
+    [TestMethod]
+    public void MultiplyNumbers() { 
+        var calculator = new Calculator();
+        var res = calculator.Multiply(10, -1, -2);
+        Assert.AreEqual(20, res);
+    }
+    [TestMethod]
+    public void DivideNumbers()
+    {
+        var calculator = new Calculator();
+        var res = calculator.Divide(20, 5, 2);
+        Assert.AreEqual(2, res);
+    }
 }
