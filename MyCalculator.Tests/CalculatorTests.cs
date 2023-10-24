86c5d9433cdd0606f4e685100d382be1a390008e
leirdan <uouleirdan@gmail.com>
2023-10-24 08:39:44 -0300

    :white_check_mark: test: adding parameterized tests

--- MyCalculator.Tests/CalculatorTests.cs
+++ MyCalculator.Tests/CalculatorTests.cs
@@ -15,18 +15,24 @@ public class CalculatorTests
         // Assert
         Assert.AreEqual(9, res);
     }
-    [TestMethod]
-    public void SubtractNumbersTest()
+    // Teste controlado por dados, ou teste parametrizado.
+    [DataTestMethod]
+    [DataRow(3, -2, 0, 5)]
+    [DataRow(1, 2, 4, -5)]
+    [DataRow(-22, -2, 20, -40)]
+    public void SubtractNumbersTest(int x, int y, int z, int expect)
     {
         var calculator = new Calculator();
-        var res = calculator.Subtract(10, 3, 5);
-        Assert.AreEqual(2, res);
+        var res = calculator.Subtract(x, y, z);
+        Assert.AreEqual(expect, res);
     }
-    [TestMethod]
-    public void MultiplyNumbersTest() { 
+    [DataTestMethod]
+    [DataRow(3, -4, -1, 12)]
+    [DataRow(6, 5, 2, 60)]
+    public void MultiplyNumbersTest(int x, int y, int z, int expect) { 
         var calculator = new Calculator();
-        var res = calculator.Multiply(10, -1, -2);
-        Assert.AreEqual(20, res);
+        var res = calculator.Multiply(x, y, z);
+        Assert.AreEqual(expect, res);
     }
     [TestMethod]
     public void DivideNumbersTest()
